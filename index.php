7e0c822774d4ccbc2cfb5906069516778a5c0acf
aaronchong <aaronchong888@gmail.com>
2017-01-22 02:43:02 +0800

    update mainpage

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
- <html>
+<!DOCTYPE html>
+<html>
   <head>
-    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SilverNeedle</title>
     <link rel="stylesheet" type="text/css"  href="style.css">
     <link rel="stylesheet" type="text/css"  href="jasny-bootstrap.css">
