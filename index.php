10ccb3ad08eac70bdae9d11e98b8e973d7ad6c9c
aaronchong <aaronchong888@gmail.com>
2017-01-22 00:41:39 +0800

    add register

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     print '<input name="password" id="loginPassword" placeholder="Password" type="password">';
     print '<input type="submit" value="Login" onclick="login()">';
     print '<a class="register" href="/register.php">Register</a>';
-    print '<br><div id="loginError"></div></div>';
+    print '</div><div id="loginError"></div>';
   }
   else{
     print '<div class="top_left">';
