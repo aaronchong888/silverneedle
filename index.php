a7e479848b0bc8684cf17a9e4a55d01310fc5024
aaronchong <aaronchong888@gmail.com>
2017-01-22 02:25:18 +0800

    update mainpage

--- index.php
+++ index.php
@@ -30,14 +30,10 @@
     print '</form>';
     print '</div>';
     print '<h2 class="heading"> What to eat next?</h2><br>';
-    print '<input type="file" accept="image/*" capture="capture">';
-    print '<div class="file-input-wrapper">';
-    print '<button class="btn-file-input">SELECT FILES</button>';
-    print '<input type="file" name="image" id="image" value="" />';      
+    print '<div class="dropZoneContainer">';
+    print '<input type="file" id="drop_zone" class="FileUpload" accept=".jpg,.png,.gif" onchange="handleFileSelect(this) " />';
+    print '<div class="dropZoneOverlay">Drag and drop your image <br />or<br />Click to add</div>';
     print '</div>';
-    print '<span id="img_text" style="float: right;';
-    print 'margin-right: -80px;';
-    print 'margin-top: -14px;"></span>';
   }
 ?>
 </div>
