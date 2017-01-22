d7ad7681209b85bbd79e279f2558ce758c7a60e8
aaronchong <aaronchong888@gmail.com>
2017-01-22 12:14:52 +0800

    update mainpage

--- js.cookie.js
+++ js.cookie.js
@@ -1,9 +1,5 @@
-
-
-
-
 <!DOCTYPE html>
-<html lang="en" class=" is-u2f-enabled">
+<html lang="en" class="is-u2f-enabled">
   <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# object: http://ogp.me/ns/object# article: http://ogp.me/ns/article# profile: http://ogp.me/ns/profile#">
     <meta charset='utf-8'>
     
